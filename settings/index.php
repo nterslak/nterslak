9ce35ba4bc22d049825928be71a3c65caa0af7c4
Konstantin Protzen <code@kondev.de>
2024-09-16 10:37:30 +0200

    ✨ Add OSS-License section

--- settings/index.php
+++ settings/index.php
@@ -8,12 +8,28 @@
     <link rel="stylesheet" href="/res/css/main.css" />
     <link rel="stylesheet" href="/res/css/grid.css" />
     <link rel="stylesheet" href="/res/css/nav.css" />
+    <link rel="stylesheet" href="/settings/settings.css">
 </head>
 
 <body>
     <?php include $_SERVER['DOCUMENT_ROOT'] . '/res/html/header.html'; ?>
     <?php include $_SERVER['DOCUMENT_ROOT'] . '/res/html/nav.html'; ?>
-    <main></main>
+    <main>
+        <div class="category_container">
+            <h1>Open-Source-Licenses</h1>
+            <p>
+                We use the following open-source projects:
+            </p>
+            <ul>
+                <li>Most of the icons come from <a href="https://heroicons.com">Heroicons</a> (<a
+                        href="https://raw.githubusercontent.com/tailwindlabs/heroicons/master/LICENSE">MIT
+                        license</a>)</li>
+            </ul>
+        </div>
+        <div class="category_container">
+            <p style="text-align: center;">Made with ❤️ in Germany</p>
+        </div>
+    </main>
     <style>
     #nav_entry_settings {
         background-color: var(--primaryColor);

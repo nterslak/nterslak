f0d654126a2004531c18ba06fb6499aaba85f9e7
Konstantin Protzen <code@kondev.de>
2024-09-16 16:25:51 +0200

    ✨ Add password changing

--- settings/index.php
+++ settings/index.php
@@ -16,14 +16,39 @@
     <?php include $_SERVER['DOCUMENT_ROOT'] . '/res/html/nav.html'; ?>
     <main>
         <div class="category_container">
+            <h1>Login-Password</h1>
+            <p>
+                Currently this software is only meant to contain one dataset per Instance and therefore does not use
+                standard accounts but only a password for login authentication.
+            </p>
+            <p>
+                Here you can change the password:
+            </p>
+            <form action="/settings/change_password.php" method="post">
+                <div class="input-container">
+                    <input type="password" autocomplete="current-password" spellcheck="false" tabindex="0"
+                        aria-label="Password" name="password" value="" autocapitalize="none" placeholder=" " required />
+                    <div class="input-tooltip" aria-hidden="true">New Password</div>
+                </div>
+                <div class="input-container">
+                    <input type="password" autocomplete="current-password" spellcheck="false" tabindex="0"
+                        aria-label="Password" name="password" value="" autocapitalize="none" placeholder=" " required />
+                    <div class="input-tooltip" aria-hidden="true">Repeat new Password</div>
+                </div>
+                <input type="submit" id="submit_button" value="Set new Password" />
+            </form>
+        </div>
+        <div class="category_container">
             <h1>Open-Source-Licenses</h1>
             <p>
                 We use the following open-source projects:
             </p>
             <ul>
-                <li>Most of the icons come from <a href="https://heroicons.com">Heroicons</a> (<a
+                <li>
+                    Most of the icons come from <a href="https://heroicons.com">Heroicons</a> (<a
                         href="https://raw.githubusercontent.com/tailwindlabs/heroicons/master/LICENSE">MIT
-                        license</a>)</li>
+                        license</a>)
+                </li>
             </ul>
         </div>
         <div class="category_container">

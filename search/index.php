f5dd49c5b59c2cebecc265cafd850aa2b1386e6e
Konstantin Protzen <code@kondev.de>
2024-09-19 16:41:27 +0200

    Add search page

---
+++ search/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <title>Nterslak</title>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/res/html/head.html'; ?>
+</head>
+
+<body>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/res/html/header.html'; ?>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/res/html/nav.html'; ?>
+    <main></main>
+    <style>
+    #nav_entry_search {
+        background-color: var(--primaryColor);
+        color: var(--black);
+    }
+    </style>
+</body>
+
+</html>

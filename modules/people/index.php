2d9cda0421d5fca0060deb30efa8f535f59b15e2
Konstantin Protzen <code@kondev.de>
2024-10-04 01:04:12 +0200

    ✨ Add edit link

--- modules/people/index.php
+++ modules/people/index.php
@@ -98,7 +98,7 @@
 
                     foreach ($people as $person) {
                         echo '<tr>';
-                        echo '<td>' . htmlspecialchars($person['name']) . '</td>';
+                        echo '<td><a href="./edit?id=personid">' . htmlspecialchars($person['name']) . '</a></td>';
                         echo '<td>' . htmlspecialchars($person['email']) . '</td>';
                         echo '<td>' . htmlspecialchars($person['phone']) . '</td>';
                         echo '</tr>';

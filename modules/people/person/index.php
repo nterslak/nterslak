b088ab7703ce8c9101a4e1e7aea05d207bce17b6
Konstantin Protzen <code@kondev.de>
2024-10-05 00:50:26 +0200

    ✨ Add masonry layout

--- modules/people/person/index.php
+++ modules/people/person/index.php
@@ -15,6 +15,33 @@
             <span class="person-name">John Doe</span>
         </div>
         <div class="person-main">
+            <div class="person-info masonry">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident non nobis voluptate dolore
+                    tempora ad optio esse nemo repellendus. Molestiae necessitatibus repellat saepe at dignissimos
+                    facere non illum minus ipsa.</p>
+                <p>Est eius impedit quo, assumenda similique cumque incidunt perspiciatis expedita aspernatur porro.
+                    Perferendis dolores nobis, voluptas saepe delectus odio hic. Odio minima a ab qui at eum neque
+                    laboriosam ea?</p>
+                <p>Delectus facere voluptates, doloremque ut iste eos ducimus rem voluptatem perferendis fugit fugiat
+                    animi sunt ad temporibus odit cumque dignissimos aspernatur eveniet sed? Corrupti itaque pariatur
+                    totam ratione quisquam ullam.</p>
+                <p>Eaque eos alias itaque nesciunt quasi cupiditate odio, sed aliquam perspiciatis? Blanditiis ab,
+                    sapiente suscipit esse nesciunt rem iure aperiam voluptatum omnis voluptas porro ut non! Facilis
+                    iste temporibus vero.</p>
+                <p>Repudiandae beatae consequuntur error fugiat.</p>
+                <p>Corrupti modi eveniet rerum tenetur nobis natus unde eligendi obcaecati dolores cupiditate quo, quam
+                    impedit repudiandae nostrum suscipit nisi iste quod minima et? Deserunt placeat corrupti porro non
+                    quasi nisi!</p>
+                <p>Illum, maiores optio temporibus labore alias cupiditate quo, pariatur dolorum aspernatur molestiae
+                    nisi amet eum sed similique iure molestias suscipit animi minus sit voluptate dicta doloribus
+                    eligendi recusandae? Et, laboriosam?</p>
+                <p>Ab veritatis at non nostrum error odio libero cum itaque nam reiciendis, nulla neque fugit. Ab
+                    ratione debitis modi! Quo debitis alias inventore quis fuga temporibus veritatis dolores nemo. Unde.
+                </p>
+            </div>
+            <div class="person-footer">
+                <a href="">+ Add information</a>
+            </div>
         </div>
     </main>
     <style>

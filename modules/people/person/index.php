e80d9c0415e14a61b6d04b95586639b53dbcef09
Konstantin Protzen <code@kondev.de>
2024-10-04 17:10:28 +0200

    ✨ Add person page

---
+++ modules/people/person/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <title>Nterslak</title>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/res/html/head.html'; ?>
+    <link rel="stylesheet" href="./style.css">
+</head>
+
+<body>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/res/html/header.html'; ?>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/res/html/nav.html'; ?>
+    <main>
+        <div class="person-nav">
+            <span class="person-name">John Doe</span>
+        </div>
+        <div class="person-main">
+        </div>
+    </main>
+    <style>
+        #nav_entry_people {
+            background-color: var(--primaryColor);
+            color: var(--black);
+        }
+    </style>
+</body>
+
+</html>

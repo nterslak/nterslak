76648e6ff2dabf7cf3255b8b3e5433e0cbc8d4d4
Konstantin Protzen <code@kondev.de>
2024-11-14 21:38:15 +0100

    ✨ Add dates table

--- modules/people/person/index.php
+++ modules/people/person/index.php
@@ -43,6 +43,26 @@
                     <h2>Notes</h2>
                     <textarea name="" id=""></textarea>
                 </div>
+                <div class="info_container info_dates">
+                    <h2>Dates</h2>
+                    <table class="info_dates-table">
+                        <tr>
+                            <td>01.01.1970</td>
+                            <td>Birthdate</td>
+                        </tr>
+                        <tr>
+                            <td>01.01.2020</td>
+                            <td>Marriage</td>
+                        </tr>
+                    </table>
+                    <div class="info_container-actions">
+                        <a href="">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                            </svg>
+                        </a>
+                    </div>
+                </div>
                 <p>Repudiandae beatae consequuntur error fugiat.</p>
                 <p>Corrupti modi eveniet rerum tenetur nobis natus unde eligendi obcaecati dolores cupiditate quo, quam
                     impedit repudiandae nostrum suscipit nisi iste quod minima et? Deserunt placeat corrupti porro non

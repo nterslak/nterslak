f0374c19c2297034f5c7e55b96f83c6dae8d4521
Konstantin Protzen <code@kondev.de>
2024-11-15 12:48:36 +0100

    ✨ Add name container

--- modules/people/person/index.php
+++ modules/people/person/index.php
@@ -17,6 +17,30 @@
         </div>
         <div class="person-main">
             <div class="person-info masonry">
+                <div class="info_container info_name">
+                    <h2>Name</h2>
+                    <table class="info-table">
+                        <tr>
+                            <td>John</td>
+                            <td>First Name</td>
+                        </tr>
+                        <tr>
+                            <td>Doe</td>
+                            <td>Last Name</td>
+                        </tr>
+                        <tr>
+                            <td>Joey</td>
+                            <td>Nickname</td>
+                        </tr>
+                    </table>
+                    <div class="info_container-actions">
+                        <a href="">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                            </svg>
+                        </a>
+                    </div>
+                </div>
                 <div class="info_container info_adress">
                     <h2>Adress</h2>
                     <p class="info-title">
@@ -45,7 +69,7 @@
                 </div>
                 <div class="info_container info_dates">
                     <h2>Dates</h2>
-                    <table class="info_dates-table">
+                    <table class="info-table">
                         <tr>
                             <td>01.01.1970</td>
                             <td>Birthdate</td>
